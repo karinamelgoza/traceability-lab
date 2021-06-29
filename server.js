655bc8ed5a8adc0afdfd0e0b1ab53ecfc6dbc5f6
Karina Melgoza <kmelgoza08@gmail.com>
2021-06-29 12:58:06 -0700

    joins index.html

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ app = express()
 
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
 
 const port = process.env.PORT || 5050
 app.listen(port, () => {
