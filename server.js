ac39dc00cdc9a0aaec32b22b0f0a0617f5924cff
Karina Melgoza <kmelgoza08@gmail.com>
2021-06-29 13:11:55 -0700

    adds false endpoint

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
+app.get('/index', (req, res) => {
+    res.data()
+})
+
 
 const port = process.env.PORT || 5050
 app.listen(port, () => {
